0e3fa86b07fe596bc10d053eeb97870d6d96ce1b
Edward Kmett <ekmett@gmail.com>
2020-02-08 03:16:18 -0800

    extend poppy past 4gb

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ extern crate bitintr;
 use bitintr::x86::bmi::{bextr};
 use bitintr::x86::bmi2::{pdep,bzhi};
 use std::iter::{IntoIterator,FromIterator};
-// use std::mem::{self, MaybeUninit};
 
 pub trait Select {
   fn select(self, count: usize) -> usize;
@@ -13,19 +12,6 @@ pub trait Select {
 pub trait Rank {
   fn rank(self, index: usize) -> usize;
 }
-
-//macro_rules! array_from_fn {
-//  ($type:ty, $size:expr, $func:expr) => {
-//    unsafe {
-//      let func = $func;
-//      let mut array: [MaybeUninit<$type>; $size] = MaybeUninit::uninit().assume_init();
-//      for i in 0..$size {
-//        std::ptr::write(&mut array[i], MaybeUninit::new(func(i))); // on any panic in here we leak, don't panic
-//      }
-//      mem::transmute::<_, [$type; $size]>(array)
-//    }
-//  };
-//}
 
 macro_rules! impl_all {
   ($impl_macro:ident: $($id:ident),*) => {
@@ -57,23 +43,37 @@ macro_rules! impl_rank_select {
 impl_all!(impl_rank_select: u8, u16, u32, u64);
 
 // small poppy structure, represents up to 2^32 entries
-struct Poppy { raw: Vec<u64>, index: Vec<u64> }
+struct Poppy { raw: Vec<u64>, huge: Vec<u64>, index: Vec<u64> }
 
 impl Poppy {
-  pub fn new(bits: Vec<u64>) -> Poppy {
-    let index = make_poppy(&bits);
-    Poppy { raw: bits, index: index }
+  pub fn new(raw: Vec<u64>) -> Poppy {
+    let len = raw.len();
+    let (q,r) = (len>>5,len&31);
+    let mut prefix_sum = 0u64;
+    let mut huge: Vec<u64> = vec![];
+    let mut index: Vec<u64> = (0..q).map(|i| {
+      if bzhi(i,27) == 0 { // this should be done by inverting this loop
+        huge.push(prefix_sum);
+        prefix_sum = 0;
+      }
+      let mut sub:[u32;4] = [0;4];
+      for j in 0..32 { sub[j>>3] += raw[(i<<5)+j].count_ones() }
+      let result = (sub[0] + (sub[1]<<10) + (sub[2]<<20)) as u64 + (prefix_sum<<32);
+      prefix_sum += (sub[0] + sub[1] + sub[2] + sub[3]) as u64;
+      // every 4gb we should clear the current prefix_sum and write it into the huge index
+      result
+    }).collect();
+    if r != 0 { // deal with any partial blocks at the end
+      if bzhi(q,27) == 0 { // this should be done by inverting this loop
+        huge.push(prefix_sum);
+        prefix_sum = 0;
+      }
+      let mut sub:[u32;4] = [0;4];
+      for j in 0..r { sub[j>>3] += raw[(q<<5)+j].count_ones() }
+      index.push((sub[0] + (sub[1]<<10) + (sub[2]<<20)) as u64 + (prefix_sum<<32));
+    }
+    Poppy { raw: raw, huge: huge, index: index }
   }
-//  pub fn push(&mut self, elem: u64) {
-//    self.raw.push(elem)
-//    let n = self.raw.len()
-//    if (n & 31 == 1) {
-//      // TODO: we need to push a new entry, grab the last entry, add the total for the final subblock,
-//      // and init this entry in the index
-//    } else if (n & 7 == 0 && n & 31 /= 0) {
-//      // TODO: update our subblock count here
-//    }
-//  }
 }
 
 impl FromIterator<u64> for Poppy {
@@ -90,33 +90,6 @@ impl IntoIterator for Poppy {
   }
 }
 
-fn vec_init<T>(count: usize, f: fn(usize) -> T) -> Vec<T> {
-  (0..count).map(f).collect()
-}
-
-fn make_poppy(raw: &Vec<u64>) -> Vec<u64> {
-  let len = raw.len();
-  let (q,r) = (len>>5,len&31);
-  let mut prefix_sum = 0u64;
-  let mut result: Vec<u64> = (0..q).map(|i| {
-    let i5 = i<<5;
-    //let sub:[u32;4] = array_from_fn!(u32,4,|j|(0usize..8usize).fold(0u32,|a,k|a+(raw[i5+(j<<3)+k].count_ones())));
-    let mut sub:[u32;4] = [0;4];
-    for j in 0..32 { sub[j>>3] += raw[i5+j].count_ones() }
-    let result = (sub[0] + (sub[1]<<10) + (sub[2]<<20)) as u64 + (prefix_sum<<32);
-    prefix_sum += (sub[0] + sub[1] + sub[2] + sub[3]) as u64;
-    result
-  }).collect();
-  if r != 0 { // deal with any partial blocks at the end
-    let mut sub:[u32;4] = [0;4];
-    for j in 0..r { sub[j>>3] += raw[q*32+j].count_ones() }
-    result.push((sub[0] + (sub[1]<<10) + (sub[2]<<20)) as u64 + (prefix_sum<<32));
-  }
-  // assert prefix_sum is 32 bits or less
-  assert_eq!(prefix_sum,bzhi(prefix_sum,32));
-  result
-}
-
 impl Rank for &Poppy {
   fn rank(self, i : usize) -> usize {
     let w = self.index[i >> 11]; // raw word from the index
@@ -126,7 +99,8 @@ impl Rank for &Poppy {
     let m = bzhi(w as u32, 10*bextr(i,9,2) as u32);
     let subblock_rank = ((m&1023) + bextr(m,10,10) + bextr(m,20,10)) as usize;
     let block_rank = (w >> 32) as usize;
-    block_rank + subblock_rank + word_rank + self.raw[base + z].rank(i & 63)
+    let huge_rank = self.huge[i >> 32] as usize; // allow structures > 4gb
+    huge_rank + block_rank + subblock_rank + word_rank + self.raw[base + z].rank(i & 63)
   }
 }
 
